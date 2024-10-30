c0546dd129ad8443338d7b8294c8352d1a11846b
dominionbanjo <dominionbanjo@gmail.com>
2024-10-30 05:07:16 +0100

    fixed dataparser import

--- src/middleware/multerMiddleware.ts
+++ src/middleware/multerMiddleware.ts
@@ -1,21 +1,20 @@
 import multer from "multer";
-import DataParser from "datauri/parser";
+import DatauriParser from "datauri/parser.js";
 import path from "path";
 
 const storage = multer.memoryStorage();
-
 const upload = multer({ storage });
 
-const parser = new DataParser();
+const parser = new DatauriParser();
 
-interface File {
+interface MulterFile {
   originalname: string;
   buffer: Buffer;
 }
 
-export const formatImage = (file: File): string => {
-  const fileExtension = path.extname(file.originalname).toString();
-  return parser.format(fileExtension, file.buffer).content as string;
+export const formatImage = (file: MulterFile): string => {
+  const fileExtension = path.extname(file.originalname);
+  return parser.format(fileExtension, file.buffer)?.content as string;
 };
 
 export default upload;
